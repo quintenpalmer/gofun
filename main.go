79f2fe92c893049d5aa592b6ec72edaafe550398
Quinten <quintenpalmer@gmail.com>
2013-06-14 20:05:29 +0000

    Update help text of main for fib

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func die(message string) {
 }
 
 func help() {
-	die("Usage : \nms <size> [rand]\nrlist <size>")
+	die("Usage : \nms <size> [rand]\nrlist <size>\nfib <size> [list]")
 }
 
 func main() {
