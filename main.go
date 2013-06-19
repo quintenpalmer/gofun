04d4a2c8c327f81ab5ee598a10547aaf40c0ebad
Quinten Palmer <qpalmer@aereo.com>
2013-06-19 15:10:40 +1000

    Clean up main and improve help text

--- main.go
+++ main.go
@@ -7,13 +7,22 @@ import (
 	"util"
 )
 
-func die(message string) {
-	fmt.Println(message)
+func help() {
+	fmt.Println("Usage : \n  ms <size> [rand]\n  rlist <size>\n  fib <size> [list]\n  pascal <size>")
 	os.Exit(1)
 }
 
-func help() {
-	die("Usage : \nms <size> [rand]\nrlist <size>\nfib <size> [list]\npascal <size>")
+func parseInt(args []string) int {
+	if len(args) < 3 {
+		fmt.Println("Must supply a size")
+		help()
+	}
+	i, err := strconv.Atoi(args[2])
+	if err != nil {
+		fmt.Println(err)
+		help()
+	}
+	return i
 }
 
 func main() {
@@ -22,15 +31,7 @@ func main() {
 		help()
 	} else {
 		if os.Args[1] == "ms" {
-			if len(os.Args) < 3 {
-				help()
-			} else {
-				var err error
-				size, err = strconv.Atoi(os.Args[2])
-				if err != nil {
-					fmt.Println(err)
-				}
-			}
+			size = parseInt(os.Args)
 			var to_sort []int
 			to_sort = make([]int,size)
 			for i:=0; i < size; i++ {
@@ -42,48 +43,28 @@ func main() {
 			fmt.Println(to_sort)
 			fmt.Println(util.Ms(to_sort))
 		} else if os.Args[1] == "rlist" {
-			if len(os.Args) < 3 {
-				help()
-			} else {
-				var err error
-				size, err = strconv.Atoi(os.Args[2])
-				if err != nil {
-					fmt.Println(err)
-				}
-				to_sort := util.Rlist(size)
-				fmt.Println(to_sort)
-			}
+			size = parseInt(os.Args)
+			to_sort := util.Rlist(size)
+			fmt.Println(to_sort)
 		} else if os.Args[1] == "fib" {
-			var err error
-			size, err = strconv.Atoi(os.Args[2])
-			if err != nil {
-				fmt.Println(err)
-			}
+			size = parseInt(os.Args)
 			if len(os.Args) > 3 && os.Args[3] == "list" {
 				fmt.Println(util.FibList(size))
 			} else {
 				fmt.Println(util.FibIndex(size))
 			}
 		} else if os.Args[1] == "pascal" {
-			var err error
-			size, err = strconv.Atoi(os.Args[2])
-			if err != nil {
-				fmt.Println(err)
-			}
-			if len(os.Args) > 3 && os.Args[3] == "list" {
-				fmt.Println(util.FibList(size))
-			} else {
-				triangle := util.Pascal(size)
-				size = len(triangle)
-				for i, line := range triangle {
-					for j := 0; j < size - i; j++ {
-						fmt.Printf("   ")
-					}
-					for _, value := range line {
-						fmt.Printf(" %0-5d",value)
-					}
-					fmt.Printf("\n")
+			size = parseInt(os.Args)
+			triangle := util.Pascal(size)
+			size = len(triangle)
+			for i, line := range triangle {
+				for j := 0; j < size - i; j++ {
+					fmt.Printf("   ")
 				}
+				for _, value := range line {
+					fmt.Printf(" %0-5d",value)
+				}
+				fmt.Printf("\n")
 			}
 		}
 	}
